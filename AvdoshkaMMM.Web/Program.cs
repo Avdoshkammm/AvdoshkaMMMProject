87ebef868d7d5aed2833551a8b66a40d525a0ce0
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-ER3FDUM>
2025-05-20 16:17:29 +0300

    Initial commit

--- AvdoshkaMMM.Web/Program.cs
+++ AvdoshkaMMM.Web/Program.cs
@@ -12,6 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//Initial commit for dev branch
 
 app.UseHttpsRedirection();
 app.UseRouting();
